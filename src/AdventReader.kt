1a3c5365ecfde5ae7de186f9b5171f1d4d56e80e
Kar Sar <sarkissova@gmail.com>
2025-03-03 23:44:09 +0100

    Working A part, will continue with B tomorrow evening.

--- src/AdventReader.kt
+++ src/AdventReader.kt
@@ -4,4 +4,8 @@ import java.io.File
 class AdventReader {
 
     fun returnData(path: String): List<String> = File(path).readLines()
+
+    fun returnFile(path: String): List<String> = File(path).readLines()
+
+
 }
---
+++ src/Aoc2020Dec04/AoC2020dec04.kt
@@ -0,0 +1,33 @@
+package Aoc2020Dec04
+
+import AdventReader
+
+
+val path1 = "src/AoCData/AoC2020Dec04"
+val adventReader1 = AdventReader()
+
+var adventInput1 = adventReader1.returnFile(path1).joinToString("\n") //konverterar listan från List String för att kunna splitta den
+
+
+fun splitNewline(): List<String> {
+    val regex = Regex("\n\n+")
+    return adventInput1.split(regex)
+
+}
+
+fun main(){
+   // println(splitNewline())
+    // gör en map av splitNewline()
+    val passportLists = splitNewline().map{it.split("\n").toMutableList()}
+    val passportKeys = passportLists.map{ passport ->
+        passport.flatMap { it.split(" ")}.map{it.split(":")[0]}.toSet()
+    }
+
+    val passportRequirements = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+
+    val validPassports=passportKeys.count{it.containsAll(passportRequirements)}
+
+    println("Valid passports:  $validPassports")
+
+
+}
