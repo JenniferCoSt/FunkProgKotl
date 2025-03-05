147e6587d182be1691968cb1d6cc877bd2ed6b4b
Kar Sar <sarkissova@gmail.com>
2025-03-05 12:00:54 +0100

    added comments to 04A

--- src/Aoc2020Dec04/AoC2020dec04A.kt
+++ src/Aoc2020Dec04/AoC2020dec04A.kt
@@ -6,25 +6,26 @@ val path1 = "src/AoCData/AoC2020Dec04"
 val adventReader1 = AdventReader()
 
 var adventInput1 = adventReader1.returnFile(path1).joinToString("\n") //konverterar listan från
-// List String för att kunna splitta den
+// List String till String och splittar den, varje rad från filen separeras med en newline.
 
-fun splitNewline(): List<String> {
-    val regex = Regex("\n\n+")
-    return adventInput1.split(regex)
+fun splitNewline(): List<String> { //denna metod använder Regular Expression för att dela upp strängen där det finns två
+    val regex = Regex("\n\n+") //eller fler newlines. Där delas passen upp mellan varandra.
+    return adventInput1.split(regex) //detta blir en lista med varje pass som en sträng i listan.
 }
 
 fun main(){
     // gör en map av splitNewline()
-    val passportLists = splitNewline().map{it.split("\n").toMutableList()}
-    val passportKeys = passportLists.map{ passport ->
-        passport.flatMap { it.split(" ")}.map{it.split(":")[0]}.toSet()
-    }
+    val passportLists = splitNewline().map{it.split("\n").toMutableList()} //skpar en lista av pass-strängar. map gör varje pass till en lista på en egen rad.
+    val passportKeys = passportLists.map{ passport ->// flatmapen delar varje rad till individuella key:values
+        passport.flatMap { it.split(" ")}.map{it.split(":")[0]}.toSet() // map-en extraherar bara Keys.
+    } //toSet konverterar listan till en set, för att skapa unika nycklar.
 
-    val passportRequirements = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid") //förenkla denna mer till java
 
-    val validPassports=passportKeys.count{it.containsAll(passportRequirements)}
+    val passportRequirements = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid") //skapar en lista av de obligatoriska fälten
 
-    println("Valid passports:  $validPassports")
+    val validPassports=passportKeys.count{it.containsAll(passportRequirements)} //räknar ut antal pass som har de obligatoriska fälten
+
+    println("Valid passports:  $validPassports") //skriver ut antal godkända pass
 
 
 }
